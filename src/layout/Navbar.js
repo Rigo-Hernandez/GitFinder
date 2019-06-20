d0344fd93c12a92e888fd46697f5f371b2be6763
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-06-19 23:45:46 -0500

    Importing components/structuring app

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -16,6 +16,7 @@ export class Navbar extends Component {
       <nav className='navbar bg-primary'>
         <h1>
           <i className={this.props.icon} /> {this.props.title}
+          
         </h1>
       </nav>
     );
