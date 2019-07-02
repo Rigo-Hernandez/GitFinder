6dd471231b2f490dc56f7aaa012641ae6dbb3f92
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-07-01 21:23:11 -0500

    added search bar and submit button, need to pass props up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './layout/Navbar'
 import Users from './components/users/Users'
+import Search from './components/users/Search'
 import axios from 'axios'
 import './App.css';
 
@@ -25,7 +26,10 @@ class App extends Component {
     return (
       <div className='App'>
         <Navbar />
+        <div className='container'>
+        < Search />
         <Users loading ={this.state.loading} users={this.state.users} />
+        </div>
       </div>
     );
   }
