4d56f86007a48eff3848876f967b065cbba27daf
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-07-03 20:59:11 -0500

    Cleaned up code to be more understandable

--- src/components/users/Useritem.js
+++ src/components/users/Useritem.js
@@ -1,22 +1,25 @@
-import React from 'react'
-import propTypes from "prop-types"
+import React from 'react';
+import propTypes from 'prop-types';
 
-const Useritem = ({user: {login, avatar_url, html_url}}) => {
-
-        return (
-            <div className= "card text-center">
-                <img src={avatar_url} className='round-img' style= {{ width: '60px'}} alt=""/>
-                <h3>{login}</h3>
-
-                <div>
-                    <a href={html_url}>More</a>
-                </div>
-            </div>
-        )
-}
+const Useritem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        className='round-img'
+        style={{ width: '60px' }}
+        alt=''
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url}>More</a>
+      </div>
+    </div>
+  );
+};
 
 Useritem.propTypes = {
-    user:propTypes.object.isRequired,
-}
+  user: propTypes.object.isRequired
+};
 
-export default Useritem
+export default Useritem;
