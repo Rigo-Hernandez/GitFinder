2c0106413045768ffae4e4f4d19d2e67313db9ee
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-07-22 22:27:57 -0500

    Added about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <Fragment>
       <h1>About this app </h1>
-      <p>A Web App to search Github users</p>
+      <p>A Web App to search Github users.</p>
       <p>Version 1.0.0</p>
     </Fragment>
   );
