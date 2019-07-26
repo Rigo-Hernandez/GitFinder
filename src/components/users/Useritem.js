cd3ef73bc740ebacc7c2b56113789476827bf8f5
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-07-25 22:40:19 -0500

    added user component where linkes will not redirect to github but fetch the data

--- src/components/users/Useritem.js
+++ src/components/users/Useritem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link }  from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const Useritem = ({ user: { login, avatar_url, html_url } }) => {
   return (
@@ -14,7 +14,7 @@ const Useritem = ({ user: { login, avatar_url, html_url } }) => {
       <h3>{login}</h3>
 
       <div>
-        <Link to={`/user/${login}`} className='btn btn-dark btn-sm my-1' >
+        <Link to={`/user/${login}`} className='btn btn-dark btn-sm my-1'>
           More
         </Link>
       </div>
