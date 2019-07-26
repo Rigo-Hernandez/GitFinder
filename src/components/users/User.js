8cd25a8f0927100534055bcba80c5edf9f683495
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-07-25 22:39:56 -0500

    added user component where linkes will not redirect to github but fetch the data

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,18 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export class User extends Component {
-    componentDidMount() {
-        this.props.getUser(this.props.match.params.login)
-    }
+  componentDidMount() {
+    this.props.getUser(this.props.match.params.login);
+  }
 
+  render() {
+    const {
+      name,
+      avatar_url,
+      bio,
+      blog,
+      login,
+      html_url,
+      followers,
+      following,
+      public_repos,
+      public_gists,
+      hireable
+    } = this.props.user;
 
-    render() {
-        return (
-            <div>
-                User
-            </div>
-        )
-    }
+    const { loading } = this.props;
+
+    return <div>{name}</div>;
+  }
 }
 
-export default User
+export default User;
