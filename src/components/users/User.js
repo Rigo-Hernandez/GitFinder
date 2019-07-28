0f934f2cb0ff57f09d53d83870d5da8548ea59ed
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-07-27 23:45:11 -0500

    Added back to search button

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,7 +1,7 @@
-import React, { Fragment,  Component } from 'react';
-import Spinner from '../../layout/Spinner'
-import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
+import React, { Fragment, Component } from 'react';
+import Spinner from '../../layout/Spinner';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export class User extends Component {
   componentDidMount() {
@@ -11,8 +11,8 @@ export class User extends Component {
   static propTypes = {
     loading: PropTypes.bool,
     user: PropTypes.object.isRequired,
-    getUser: PropTypes.func.isRequired,
-  }
+    getUser: PropTypes.func.isRequired
+  };
 
   render() {
     const {
@@ -31,15 +31,15 @@ export class User extends Component {
 
     const { loading } = this.props;
 
-    if(loading) return <Spinner />;
+    if (loading) return <Spinner />;
 
     return (
-    <Fragment>
-      <Link to='/' className='btn btn-light'>
-      Back To Search
-      </Link>
-    </Fragment>
-    )
+      <Fragment>
+        <Link to='/' className='btn btn-light'>
+          Back To Search
+        </Link>
+      </Fragment>
+    );
   }
 }
 
