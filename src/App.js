db4a5938b05686f92f4d2fa7f1fef285ea377c34
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-07-28 21:42:34 -0500

    Added user repo information and crated repo folder

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ class App extends Component {
     users: [],
     user: {},
     loading: false,
-    alert: null
+    alert: null,
+    repos: []
   };
   ///  component loads github users on start, can be commented or removed
 
@@ -52,6 +53,20 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
   };
 
+  //Get Users Repo
+  getUserRepos = async username => {
+    this.setState({ loading: 'true' });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ repos: res.data, loading: false });
+  };
+
+
   // clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
   // Set Alert
@@ -62,7 +77,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, user, loading } = this.state;
+    const { users, user, repos,  loading } = this.state;
     return (
       <Router>
         <div className='App'>
@@ -93,7 +108,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
