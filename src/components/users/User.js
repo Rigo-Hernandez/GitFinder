55fa5e1b2037f77cf15d47782fc350e22d662758
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-07-28 20:59:14 -0500

    Added bio information

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -20,6 +20,7 @@ export class User extends Component {
       avatar_url,
       bio,
       blog,
+      company,
       login,
       location,
       html_url,
@@ -55,6 +56,36 @@ export class User extends Component {
             />
             <h1>{name}</h1>
             <p>Location: {location}</p>
+          </div>
+          <div>
+            {bio && (<Fragment>
+              <h3>Bio</h3>
+              <p>{bio}</p>
+            </Fragment>
+            )}
+            <a href={html_url} className='btn btn-dark my-1'>
+              Visit {name}'s Github
+            </a>
+            <ul>
+              <li>
+                {login && <Fragment>
+                  <strong>Username: </strong> {login}
+                </Fragment>}
+              </li>
+              <li>
+                {company && <Fragment>
+                  <strong>Company: </strong> {company}
+                </Fragment>}
+              </li>
+              <li>
+                {blog && <Fragment>
+                  <strong>Website: </strong> {blog}
+                </Fragment>}
+              </li>
+              <li>
+
+              </li>
+            </ul>
           </div>
         </div>
       </Fragment>
