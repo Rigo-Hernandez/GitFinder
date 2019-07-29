4c42e6b96d3ceb8ec31619ebd5dfba3ee0611629
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-07-28 20:59:33 -0500

    Added bio information

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -58,33 +58,38 @@ export class User extends Component {
             <p>Location: {location}</p>
           </div>
           <div>
-            {bio && (<Fragment>
-              <h3>Bio</h3>
-              <p>{bio}</p>
-            </Fragment>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
             )}
             <a href={html_url} className='btn btn-dark my-1'>
               Visit {name}'s Github
             </a>
             <ul>
               <li>
-                {login && <Fragment>
-                  <strong>Username: </strong> {login}
-                </Fragment>}
+                {login && (
+                  <Fragment>
+                    <strong>Username: </strong> {login}
+                  </Fragment>
+                )}
               </li>
               <li>
-                {company && <Fragment>
-                  <strong>Company: </strong> {company}
-                </Fragment>}
+                {company && (
+                  <Fragment>
+                    <strong>Company: </strong> {company}
+                  </Fragment>
+                )}
               </li>
               <li>
-                {blog && <Fragment>
-                  <strong>Website: </strong> {blog}
-                </Fragment>}
+                {blog && (
+                  <Fragment>
+                    <strong>Website: </strong> {blog}
+                  </Fragment>
+                )}
               </li>
-              <li>
-
-              </li>
+              <li />
             </ul>
           </div>
         </div>
