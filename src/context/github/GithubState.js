6ce2c626d2e4c42cb44f71bfa079445d0ceecf93
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-08-03 21:17:32 -0500

    implementing useContext

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -30,14 +30,16 @@ const GithubState = props => {
 
   //Set  Loading
 
-  return (
-    <GithubContext.Provider
-      value={{
-        users: state.users,
-        user: state.user,
-        repos: state.repos,
-        loading: state.loading
-      }}
-    />
-  );
+  return;
+  <GithubContext.Provider
+    value={{
+      users: state.users,
+      user: state.user,
+      repos: state.repos,
+      loading: state.loading
+    }}
+  >
+    {props.children}
+  </GithubContext.Provider>;
 };
+export default GithubState;
