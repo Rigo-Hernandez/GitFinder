b2f7861a573fa4f23f1a5a49846da757ab613d5f
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-08-03 21:15:08 -0500

    implementing useContext

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -1,45 +1,43 @@
-import React, { useReducer } from 'react'
+import React, { useReducer } from 'react';
 import axios from 'axios';
 import GithubContext from './githubContext';
-import GithubReducer from './githubReducer'
+import GithubReducer from './githubReducer';
 import {
-SEARCH_USERS,
-SET_LOADING,
-CLEAR_USERS,
-GET_USERS,
-GET_REPOS
-}from '../types'
-
+  SEARCH_USERS,
+  SET_LOADING,
+  CLEAR_USERS,
+  GET_USERS,
+  GET_REPOS
+} from '../types';
 
 const GithubState = props => {
-    const initialState = {
-        users: [],
-        user:{},
-        repos: [],
-        loading:false
-    }
+  const initialState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false
+  };
 
+  const [state, dispatch] = useReducer(GithubReducer, initialState);
 
-    const [ state, dispatch] = useReducer(GithubReducer,initialState)
+  // Search Users
 
-    // Search Users
+  //Get User
 
-    //Get User
+  //Get Repo
 
-    //Get Repo
+  //Clear Users
 
-    //Clear Users 
+  //Set  Loading
 
-    //Set  Loading
-
-    return <GithubContext.Provider
-    value={{
-        users:state.users,
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
         user: state.user,
-        repos:state.repos,
+        repos: state.repos,
         loading: state.loading
-    }}
-    >
-
-    </GithubContext.Provider>
-}
+      }}
+    />
+  );
+};
