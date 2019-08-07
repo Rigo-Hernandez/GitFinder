14dd667cfd3486c16b68720c887d1f3c7e0e4839
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-08-06 22:06:26 -0500

    implementing useContext

--- src/App.js
+++ src/App.js
@@ -19,19 +19,6 @@ const App = () => {
 
 
 
-  //Search github users
-  const searchUsers = async text => {
-    SetLoading(true);
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${
-        process.env.REACT_APP_GITHUB_CLIENT_ID
-      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-
-    setUsers(res.data.items);
-    SetLoading(false);
-  };
-
   //Get single github user
   const getUser = async username => {
     SetLoading(true);
@@ -83,7 +70,6 @@ const App = () => {
               render={props => (
                 <Fragment>
                   <Search
-                    searchUsers={searchUsers}
                     clearUsers={clearUsers}
                     showClear={users.length > 0 ? true : false}
                     setAlert={showAlert}
