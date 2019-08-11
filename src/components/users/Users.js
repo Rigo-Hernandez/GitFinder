aa010c8b259ac07ed7c76447ec4c80826e70b9ba
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-08-10 23:19:59 -0500

    Moving user State to Context

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,12 +1,12 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import Useritem from './Useritem';
 import Spinner from '../../layout/Spinner';
-import GithubContext from '../../context/github/githubContext'
+import GithubContext from '../../context/github/githubContext';
 
-const Users = ( ) => {
-  const githubContext = useContext(GithubContext)
+const Users = () => {
+  const githubContext = useContext(GithubContext);
 
-  const { loading, users} = githubContext
+  const { loading, users } = githubContext;
 
   if (loading) {
     return <Spinner />;
@@ -25,7 +25,6 @@ const userStyle = {
   display: 'grid',
   gridTemplateColumns: 'repeat(3,1fr)',
   gridGap: '1rem'
-  
 };
 
 export default Users;
